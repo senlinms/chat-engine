800194ebde7aa6a459cbd2350c58b8b33259a0dc
Max Presman <max@pubnub.com>
2017-09-20 23:42:32 -0700

    add travis

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const package = require('./package.json');
+const eslint = require('gulp-eslint');
 
 // task
 gulp.task('compile', function () {
@@ -17,7 +18,16 @@ gulp.task('compile', function () {
 
 });
 
+gulp.task('lint_code', [], () => {
+  return gulp.src(['src/**/*.js'])
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+});
+
 gulp.task('default', ['compile']);
+gulp.task('lint', ['lint_code']);
+gulp.task('test', ['lint']);
 
 gulp.task('watch', function() {
   gulp.watch('./src/*', ['compile']);
